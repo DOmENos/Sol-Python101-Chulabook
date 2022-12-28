5649a9aa7b43827de6010752998ec4436073ebc4
DOmENos <121551103+DOmENos@users.noreply.github.com>
2022-12-28 18:02:13 +0700

    Update Problem04.py

--- 01 Data Type, Variable and Expression/Problem04.py
+++ 01 Data Type, Variable and Expression/Problem04.py
@@ -1,5 +1,5 @@
-v1,v2,v3 = [int(i) for i in input().split()]
-u1,u2,u3 = [int(j) for j in input().split()]
+v1,v2,v3 = [float(i) for i in input().split()]
+u1,u2,u3 = [float(j) for j in input().split()]
 
 uv = (v1*u1) + (v2*u2) + (v3*u3)
 print(uv)
