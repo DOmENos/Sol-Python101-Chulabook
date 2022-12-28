a3c41970f84a6317e84d37a77c5339dd98a4c63c
DOmENos <121551103+DOmENos@users.noreply.github.com>
2022-12-28 18:02:47 +0700

    Update Problem05.py

--- 01 Data Type, Variable and Expression/Problem05.py
+++ 01 Data Type, Variable and Expression/Problem05.py
@@ -1,5 +1,5 @@
 import math
-x1,y1,x2,y2 = [int(i) for i in input().split()]
+x1,y1,x2,y2 = [float(i) for i in input().split()]
 distance = math.sqrt(math.pow(x1-x2,2) + math.pow(y1-y2,2))
 
 print(distance)
